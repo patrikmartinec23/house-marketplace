ba93e2880e9bcc671a18ff5d2a8d892aed2ba976
patrikmartinec23 <patrik.martinec1@gmail.com>
2025-05-14 23:05:45 +0200

    bug fix

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStatus } from '../hooks/useAuthStatus';
-import Spinner from './spinner';
+import Spinner from './Spinner';
 
 const PrivateRoute = () => {
     const { loggedIn, loading } = useAuthStatus();
